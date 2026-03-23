915f435865db88b82bfe63b8914c4ba403f37d6d
lyng28 <thuly2000@gmail.com>
2026-03-22 22:34:59 -0600

    Complete solution for Question 3

--- DATA221_Assignment4_Question3.py
+++ DATA221_Assignment4_Question3.py
@@ -22,3 +22,17 @@ test_accuracy_score = accuracy_score(y_test, y_test_prediction)
 print('Train accuracy: ', train_accuracy_score)
 print('Test accuracy: ', test_accuracy_score)
 
+# learn code from https://stackoverflow.com/questions/23900080/how-are-feature-importances-ordered-in-scikit-learns-randomforestregressor
+# build list of feature name and important score
+important_features = list(zip(breast_cancer_dataset.feature_names, constrained_tree_classifier.feature_importances_))
+important_features.sort(key=lambda pair: pair[1], reverse=True) # sort the list
+
+print('Top five most important features:')
+rank = 1
+for feature_name, feature_score in important_features[:5]:  # loop through top five features.
+    print(f'{rank}. {feature_name}: {feature_score:.4f}')  # print feature rank, name, and score.
+    rank+= 1
+
+# Controlling model complexity reduces overfitting by stopping tree from learning tiny specific patterns
+# Feature importance supports interpretability because it shows which inputs had the strongest impact on decision
+
